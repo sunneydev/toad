d958a0859074092b8ef64c76cf796eb4e49beddc
sunney <sunneydev@icloud.com>
2023-06-02 09:10:17 +0400

    add support for caddy config

--- apps/server/src/utils.ts
+++ apps/server/src/utils.ts
@@ -1,4 +1,5 @@
 import { IToadConfig } from "./types.js";
+import { requests } from "@sunney/requests";
 import crypto from "node:crypto";
 import os from "node:os";
 import * as fs from "node:fs/promises";
@@ -48,4 +49,27 @@ export async function listProjects(pm: ProcessManager) {
   const projects = await Promise.all(promises);
 
   return projects;
+}
+
+export async function addCaddyConfig(domain: string, port: number) {
+  const config = {
+    match: [{ host: [domain] }],
+    handle: [
+      {
+        handler: "reverse_proxy",
+        upstreams: [{ dial: `127.0.0.1:${port}` }],
+      },
+    ],
+  };
+
+  await requests.post(
+    "http://localhost:2019/config/apps/http/servers/srv0/routes",
+    { body: config }
+  );
+}
+
+export async function getCaddyConfig() {
+  return requests.get(
+    "http://localhost:2019/config/apps/http/servers/srv0/routes"
+  );
 }
