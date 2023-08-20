b9d3ce8deb1c1090e5a91c3d31d73ac654162ab4
sunneydev <sunneydev@icloud.com>
2023-08-20 17:12:14 +0400

    fix: exit after console log

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,8 +6,8 @@ const command = process.argv[process.argv.length - 1];
 
 if (command === "setup") {
   const token = conf.store.token;
-
   console.log(`Your token is: ${token}`);
+  process.exit(0);
 } else {
   start();
 }
